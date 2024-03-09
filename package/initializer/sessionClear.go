2d20ca4e030e7def75c4b74d02c4da6638563be6
NUHMANUDHEENT <nuhmanudheent@gmail.com>
2024-03-09 08:56:40 +0530

    sessio

--- package/initializer/sessionClear.go
+++
@@ -1,9 +0,0 @@
-package initializer
-
-// func SessionClear() {
-// 		session := sessions.Default(c)
-// 		fmt.Println("----------", session, "------------")
-// 		session.Delete("user")
-// 		session.Delete("admin")
-// 		session.Save()
-// 	}
